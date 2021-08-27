912a08e974553713afe9991777cf03e4fff9d631
FurqanDar <dar@wustl.edu>
2021-08-27 14:54:03 -0500

    Going back to -03 rather than Ofast

--- src/compile.sh
+++ src/compile.sh
@@ -1,4 +1,4 @@
-gcc -march=x86-64 -Ofast -std=gnu11 -lm -o lassi main.c parsekey.c print.c initialize.c structure.c energy.c cluster.c mcmove.c
+gcc -march=x86-64 -O3 -std=gnu11 -lm -o lassi main.c parsekey.c print.c initialize.c structure.c energy.c cluster.c mcmove.c
 
 gcc -march=x86-64 -O0 -g3 -std=gnu11 -lm -o lassi_gdb main.c parsekey.c print.c initialize.c structure.c energy.c cluster.c mcmove.c
 
